16e95d6c3484295866f8459c7ed1aef1160bf731
Tupshin Harper <tupshin@tupshin.com>
2015-05-01 11:12:19 -0400

    working method of passing session into multiple closures

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate cql_ffi;
 
 use std::collections::HashMap;
 use std::io::Cursor;
+use std::sync::Arc;
 use std::sync::mpsc::channel;
 
 use cql_ffi::{CassCluster,CassSession,CassError,CassPrepared};
@@ -22,16 +23,22 @@ fn main() {
 
     let session_future = CassSession::new().connect(cluster).wait();
     
-    let all_prepared:Vec<CassPrepared> = Vec::new();
 
     match session_future {
    		Err(err) => panic!("{:?}",err),
         Ok(session) => {
+		    let all_prepared1:Arc<Vec<CassPrepared>> = Arc::new(Vec::new());
+		    let all_prepared2:Arc<Vec<CassPrepared>> = all_prepared1.clone();
+		    
 			println!("session established");
-			
+			let session = Arc::new(session);
+			let prepare_session = session.clone();
+			let execute_session = session.clone();
 		    let mut router = RouteBuilder::new();
-    		router.get("/prepare/:statement", |r: &mut Request| prepare(r, &session, &all_prepared));
-    		router.get("/execute/:statement_id", |r: &mut Request| execute(r, &session, &all_prepared));
+		    
+		    router.get("/prepare/:statement", move |r: &mut Request| prepare(r, &prepare_session, &all_prepared1));
+    		router.get("/execute/:statement_id", move |r: &mut Request| execute(r, &execute_session, &all_prepared2));
+    		
     		let _server = Server::start(Config { port: 8888, threads: 1 }, router);
     		let (_tx, rx) = channel::<()>();
     		rx.recv().unwrap();
@@ -39,14 +46,14 @@ fn main() {
     }
 }
 
-fn prepare(req: &mut Request, session: &CassSession, all_prepared:&Vec<CassPrepared>) -> Result<Response,CassError> {
+fn prepare(req: &mut Request, session: &Arc<CassSession>, all_prepared: &Arc<Vec<CassPrepared>>) -> Result<Response,CassError> {
 	let statement = req.params().find("statement").unwrap();
 	let prepared:CassPrepared = try!(try!(session.prepare(statement)).wait());
     let bytes = format!("Preparing: {}!", statement).into_bytes();
      Ok(response(200, HashMap::new(), Cursor::new(bytes)))
 }
 
-fn execute(req: &mut Request, session: &CassSession, all_prepared:&Vec<CassPrepared>) -> Result<Response,CassError> {
+fn execute(req: &mut Request, session: &CassSession, all_prepared: &Arc<Vec<CassPrepared>>) -> Result<Response,CassError> {
 	let statement = req.params().find("statement").unwrap();
 	let prepared:CassPrepared = try!(try!(session.prepare(statement)).wait());
 	//prepared.
